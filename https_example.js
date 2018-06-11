527314392504c77b9b9044a64adfa5d885102128
Eliza Moore <elzmoore13@gmail.com>
2018-06-11 00:43:36 +0000

    fixed bugs and updated to request Monty Python script

--- https_example.js
+++ https_example.js
@@ -1,23 +1,24 @@
 var https = require('https');
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 }
 
 // called by https when the rqeust is made
 var callback = function(response) {
   console.log('In response handler callback!');
 
-  response.on('data', funciton(chunk){
+  response.on('data', function(chunk) {
     console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
     console.log(chunk.toString());
   });
 }
 
+
 console.log("I'm about to make the request");
 
-https.request(options.callback).end();
+https.request(options, callback).end();
 
 
 console.log("I've made the request'");
